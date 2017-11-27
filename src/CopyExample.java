89f7832e241051c57b277817292881a63dda558a
Nagesh S <Nagesh S@Nagesh-GT7CM2Q9>
2017-11-27 11:11:23 +0530

    Core Java Assignment 8.1

---
+++ src/CopyExample.java
@@ -0,0 +1,53 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
+ 
+public class CopyExample 
+{
+
+    public static void main(String[] args)
+    {	
+ 
+    	FileInputStream instream = null;
+    	FileOutputStream outstream = null;
+ 
+
+    	try{
+
+    	    File infile =new File("C:\\Users\\Nagesh S\\Downloads\\Acad Gild Course\\AcadGild Java\\MyInputFile.txt");
+    	    File outfile =new File("C:\\Users\\Nagesh S\\Downloads\\Acad Gild Course\\AcadGild Java\\MyOutputFile.txt");
+ 
+
+    	    outfile.createNewFile();
+    	    
+
+    	    instream = new FileInputStream(infile);
+    	    outstream = new FileOutputStream(outfile);
+ 
+
+    	    byte[] buffer = new byte[1024];
+ 
+
+    	    int length;
+    	    while ((length = instream.read(buffer)) > 0){
+    	    	outstream.write(buffer, 0, length);
+    	    }
+
+
+    	    instream.close();
+    	    outstream.close();
+
+
+    	    System.out.println("File copied successfully!!");
+ 
+    	    
+    	}
+
+    	catch(IOException ioe){
+    		ioe.printStackTrace();
+    	 }
+    }
+}
